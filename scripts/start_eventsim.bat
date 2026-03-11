29fc0ea2f01905f391b40bb884e580d59d40800f
kanedinh <inhxuankhang@gmail.com>
2026-03-11 19:53:26 +0700

    fix: reduce event generated

--- scripts/start_eventsim.bat
+++ scripts/start_eventsim.bat
@@ -1,9 +1,9 @@
 @echo off
 
 ::
-set DAY_FROM_NOW=100
-set NUSERS=100
-set GROWTH_RATE=10
+set DAY_FROM_NOW=321
+set NUSERS=918
+set GROWTH_RATE=0.1
 
 echo Running eventsim in detached mode...
 cd ..\eventsim
@@ -13,7 +13,7 @@ echo Started streaming events for '%NUSERS%' users...
 docker run -it ^
     --network host ^
     events ^
-    -c "../eventsim/examples/example-config.json" ^
+    -c "examples/example-config.json" ^
     -f %DAY_FROM_NOW% ^
     --nusers %NUSERS% ^
     --growth-rate %GROWTH_RATE% ^
